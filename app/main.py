64cb936b6d0f7380248aa8864ded80300a327f46
Serhii <serhijkrasa64uk5@gmail.com>
2024-09-25 13:13:58 +0300

    Better Soltuin

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": error.get("code"),
         "source": "flake8"
     }
 
@@ -17,4 +17,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(path, errors) for path, errors in linter_report.items()]
+    return [
+        format_single_linter_file(path, errors)
+        for path, errors in linter_report.items()
+    ]
